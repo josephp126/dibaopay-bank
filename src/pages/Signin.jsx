fa85a656a0625d567855273c61b34cdc8dc6993e
winnder <linodeBp17@gmail.com>
2022-08-21 21:12:12 -0700

    chnage login

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -45,18 +45,18 @@ const SignIn = () => {
   const navigate = useNavigate();
 
   const handleOpen = async () => {
-    // const user = await axios.post("http://66.94.98.22:5000/users/login", {
-    //   data: { name: account, password: password },
-    // });
-    // if (user.data != "invalid user") {
-    //   localStorage.setItem("dibao_login", user.data.token);
-    //   localStorage.setItem("dibao_username", user.data.name);
-    //   localStorage.setItem("dibao_userId", user.data.id);
-    //   navigate("/dashboard");
-    // }
-    if (account == "test" && password == "test") {
+    const user = await axios.post("http://66.94.98.22:5000/users/login", {
+      data: { name: account, password: password },
+    });
+    if (user.data != "invalid user") {
+      localStorage.setItem("dibao_login", user.data.token);
+      localStorage.setItem("dibao_username", user.data.name);
+      localStorage.setItem("dibao_userId", user.data.id);
       navigate("/dashboard");
     }
+    // if (account == "test" && password == "test") {
+    //   navigate("/dashboard");
+    // }
     if (account === "") {
       setValue("Account is not entered");
     } else if (password === "") {
